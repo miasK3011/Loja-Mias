440cc78652527c1e55bd926b379d2098d9300550
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-04-23 22:02:57 -0300

    Lógica para cadastro de produto

--- src/app/restrito/cadastro-produto/cadastro-produto.component.ts
+++ src/app/restrito/cadastro-produto/cadastro-produto.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Produto } from '../../models/Produto.model';
+import { ProdutoService } from '../../produto.service';
 @Component({
   selector: 'app-cadastro-produto',
   templateUrl: './cadastro-produto.component.html',
-  styleUrl: './cadastro-produto.component.css'
+  styleUrls: ['./cadastro-produto.component.css']
 })
-export class CadastroProdutoComponent {
-
+export class CadastroProdutoComponent implements OnInit {
+  public produto: Produto = new Produto(0, "", "", "", 0);
+  constructor(private _produtoService: ProdutoService, private router: Router) { }
+  ngOnInit(): void {
+  }
+  cadastrar() {
+    this._produtoService.cadastrarProduto(this.produto).subscribe(
+      produto => {
+        this.produto = new Produto(0, "", "", "", 0);
+        alert("Cadastro Efetuado com Sucesso")
+      },
+      err => {
+        alert("erro ao cadastrar")
+      }
+    );
+    this.router.navigate(["/restrito/lista"]);
+  }
 }
