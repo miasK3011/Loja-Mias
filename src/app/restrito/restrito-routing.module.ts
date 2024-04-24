1b66dfb0f16fa1c0ffd54af61cbf02b1847ebe7c
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-04-23 22:35:45 -0300

    Lógica de login e tokens

--- src/app/restrito/restrito-routing.module.ts
+++ src/app/restrito/restrito-routing.module.ts
@@ -6,21 +6,21 @@ import { AtualizaProdutoComponent } from './atualiza-produto/atualiza-produto.co
 import { CadastroProdutoComponent } from './cadastro-produto/cadastro-produto.component';
 import { ListaProdutoComponent } from './lista-produto/lista-produto.component';
 import { RestritoComponent } from '../restrito/restrito.component';
-const routes: Routes = [
-    { path: 'inicio', component: InicioComponent },
-    { path: 'login', component: LoginComponent },
+import { GuardGuard } from '../guard.guard';
+const restritoRoutes: Routes = [
     {
-        path: 'restrito', component: RestritoComponent,
+        path: 'restrito', component: RestritoComponent, canActivate: [GuardGuard],
+        
         children: [
-            { path: 'cadastro', component: CadastroProdutoComponent },
-            { path: 'lista', component: ListaProdutoComponent },
-            { path: 'editar/:id', component: AtualizaProdutoComponent }
+            { path: 'cadastro', component: CadastroProdutoComponent, canActivate: [GuardGuard] },
+            { path: 'lista', component: ListaProdutoComponent, canActivate: [GuardGuard] },
+            { path: 'editar/:id', component: AtualizaProdutoComponent, canActivate: [GuardGuard] }
         ]
     },
     { path: '', redirectTo: '/inicio', pathMatch: 'full' }
 ]
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(restritoRoutes)],
     exports: [RouterModule]
 })
 export class RestritoRoutingModule { }
