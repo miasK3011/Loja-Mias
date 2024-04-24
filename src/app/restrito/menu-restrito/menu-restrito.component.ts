23ee25e5c00bcda3290e5abbda2bd620bced8da0
Neemias Calebe <neemiascalebe2014@gmail.com>
2024-04-23 22:51:23 -0300

    Lógica de logout

--- src/app/restrito/menu-restrito/menu-restrito.component.ts
+++ src/app/restrito/menu-restrito/menu-restrito.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { LoginService } from '../../login.service';
 @Component({
   selector: 'app-menu-restrito',
   templateUrl: './menu-restrito.component.html',
-  styleUrl: './menu-restrito.component.css'
+  styleUrls: ['./menu-restrito.component.css']
 })
 export class MenuRestritoComponent {
-
+  constructor(private router: Router,
+    private _loginService: LoginService) { }
+  logout() {
+    localStorage.clear();
+    this._loginService.setMostraMenu(true);
+    this.router.navigate(['/login']);
+  }
 }
